689ebfc6521dc67086284220b8846a18186cb7b3
Maede eftekhari <maedehe7@gmail.com>
2024-11-04 11:17:46 +0330

    add comp

--- src/pages/MentalHealth/index.jsx
+++ src/pages/MentalHealth/index.jsx
@@ -33,64 +33,68 @@ import styles from './styles.module.css';
 import TextGuide from './TextGuide';
 
 export default function MentalHealth() {
-    const {
-        watch,
-        register,
-        setValue,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
-        mode: "all",
-    })
+    const { watch, register, setValue, handleSubmit, formState: { errors } } = useForm({ mode: "all" });
+    const [submitLoading, setSubmitLoading] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
+
     useEffect(() => {
-        register("10437", {
-            required: true,
-        })
+        register("10437", { required: true });
+        register("10438"); 
+        register("10441"); 
+        register("12504"); 
+        register("10440"); 
+    }, [register]);
 
-    }, [register])
-
-    const [submitLoading, setSubmitLoading] = useState(false)
-    const [openModal, setOpenModal] = useState(false)
     useEffect(() => {
-        fetchData(MH_JobId_Get, KEYS, setValue)
-    }, [])
+        fetchData(MH_JobId_Get, KEYS, setValue);
+    }, []);
 
-    const onSubmit = (data) => {
-        setSubmitLoading(true)
-        submitForm(MH_JobId_Post, data, () =>
-            fetchData(MH_JobId_Get, KEYS, setValue)
-        )
-            .then(() => {
-                toast.success("اطلاعات با موفقیت ذخیره شد.")
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-            .finally(() => {
-                setTimeout(() => {
-                    setSubmitLoading(false)
-                }, 1001)
-            })
-    }
+    const onSubmit = async (data) => {
+        setSubmitLoading(true);
+        try {
+            await submitForm(MH_JobId_Post, data, () => fetchData(MH_JobId_Get, KEYS, setValue));
+            toast.success("اطلاعات با موفقیت ذخیره شد.");
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setTimeout(() => setSubmitLoading(false), 1001);
+        }
+    };
+
+    useEffect(() => {
+       
+        if (!watch("10437")?.includes("10652")) {
+            setValue("10438", ""); 
+        }
+        if (!watch("10437")?.includes("1514109071882") && !watch("10437")?.includes("1514109106067")) {
+            setValue("10441", ""); 
+        }
+        
+    }, [watch("10437"), setValue]);
+
+    useEffect(() => {
+         
+        if (watch("11892") !== "10361") {
+            setValue("12504", ""); 
+            setValue("10440", ""); 
+        }
+       
+    }, [watch("11892"), setValue]);
+
     return (
         <>
             <form className={"form"} onSubmit={handleSubmit(onSubmit)}>
-                <main className={classNames(" py-2", styles.container)}id="formContainer">
-
+                <main className={classNames("py-2", styles.container)} id="formContainer">
                     <section className={styles.titleSection}>
                         <p className={styles.description}>{text.description}</p>
                     </section>
 
-                    <div
-                        className={classNames(
-                            styles.gridContainer,
-                            watch("10437")?.includes("1514109071882") ||
-                                watch("10437")?.includes("1514109106067") ||
-                                watch("10437")?.includes("10652")
-                                ? "bg-gray-e7 rounded-t px-1 pb-3 md:pb-5 mb-0"
-                                : ""
-                        )}
-                    >
+                    <div className={classNames(
+                        styles.gridContainer,
+                        watch("10437")?.includes("1514109071882") ||
+                        watch("10437")?.includes("1514109106067") ||
+                        watch("10437")?.includes("10652") ? "bg-gray-e7 rounded-t px-1 pb-3 md:pb-5 mb-0" : ""
+                    )}>
                         <CheckBoxGroup
                             questionKey="10437"
                             labelClassName={"min-w-fit"}
@@ -104,7 +108,7 @@ export default function MentalHealth() {
                             wrap
                         />
 
-                        {watch("10437")?.includes("10652") ? (
+                        {watch("10437")?.includes("10652")? (
                             <TextField
                                 containerClassName="input-card !flex-row items-center rounded p-3"
                                 className={"xs:w-[70%] md:w-[85%]"}
@@ -114,13 +118,12 @@ export default function MentalHealth() {
                                 questionKey="10438"
                                 register={register}
                                 value={watch("10438")}
-                                watch={watch}
                                 required
+                                watch={watch}
                             />
-                        ) : null}
+                        ):null}
 
-                        {watch("10437")?.includes("1514109071882") ||
-                            watch("10437")?.includes("1514109106067") ? (
+                        {(watch("10437")?.includes("1514109071882") || watch("10437")?.includes("1514109106067")) ? (
                             <RadioOptions
                                 containerClassName="input-card animate-flipLeft"
                                 label={text.historyOfSuicideAttempts}
@@ -133,17 +136,10 @@ export default function MentalHealth() {
                                 className={styles.questionBox}
                                 labelClassName={"lg:w-[220px]"}
                             />
-                        ) : null}
+                        ):null}
                     </div>
-                    <div
-                        className={classNames(
-                            styles.gridContainer,
-                            "pt-2 transition-all mb-3  px-1",
-                            watch("11892") === "10361"
-                                ? "bg-gray-e7 rounded-t pb-5 mb-0"
-                                : ""
-                        )}
-                    >
+
+                    <div className={classNames(styles.gridContainer, "pt-2 transition-all mb-3 px-1", watch("11892") === "10361" ? "bg-gray-e7 rounded-t pb-5 mb-0" : "")}>
                         <RadioOptions
                             containerClassName="input-card"
                             label={text.currentlyHavingSuicidalThoughts}
@@ -152,74 +148,47 @@ export default function MentalHealth() {
                             questionKey={"11892"}
                             register={register}
                             wrap={true}
-                            // required\
                             isError={!!errors[11892]}
-
                             required={true}
-
                             errors={errors}
                             labelClassName={"lg:w-[220px]"}
                         />
 
                         {watch("11892") === "10361" ? (
-                            <RadioOptions
-                                label={
-                                    text.willingnessToTalkWithHealthConsultant
-                                }
-                                options={yesNoQuestion}
-                                active={watch("12504")}
-                                questionKey={"12504"}
-                                register={register}
-                                wrap={true}
-                                containerClassName="input-card"
-                                labelClassName={"lg:w-[220px]"}
-                            />
-                        ) : null}
-                        {watch("11892") === "10361" ? (
-                            <RadioOptions
-                                containerClassName="input-card"
-                                label={text.historyOfThinkingSuicideAttempts}
-                                options={yesNoQuestion}
-                                active={watch("10440")}
-                                questionKey={"10440"}
-                                register={register}
-                                wrap={true}
-                                labelClassName={"lg:w-[220px]"}
-                            />
-                        ) : null}
+                            <>
+                                <RadioOptions
+                                    label={text.willingnessToTalkWithHealthConsultant}
+                                    options={yesNoQuestion}
+                                    active={watch("12504")}
+                                    questionKey={"12504"}
+                                    register={register}
+                                    wrap={true}
+                                    containerClassName="input-card"
+                                    labelClassName={"lg:w-[220px]"}
+                                />
+                                <RadioOptions
+                                    containerClassName="input-card"
+                                    label={text.historyOfThinkingSuicideAttempts}
+                                    options={yesNoQuestion}
+                                    active={watch("10440")}
+                                    questionKey={"10440"}
+                                    register={register}
+                                    wrap={true}
+                                    labelClassName={"lg:w-[220px]"}
+                                />
+                            </>
+                        ):null}
                     </div>
 
-                    <K6Test
-                        errors={errors}
-                        watch={watch}
-                        register={register}
-                        setValue={setValue}
+                    <K6Test errors={errors} watch={watch} register={register} setValue={setValue} />
+                    <DomesticViolence errors={errors} watch={watch} register={register} setValue={setValue} />
 
-                    />
-
-                    <DomesticViolence
-                        errors={errors}
-                        watch={watch}
-                        register={register}
-                        setValue={setValue}
-
-                    />
-                    <Button
-                        className={styles.submit}
-                        title="ذخیره اطلاعات"
-                        style="outlined"
-                        type="submit"
-                        loading={submitLoading}
-                    />
+                    <Button className={styles.submit} title="ذخیره اطلاعات" style="outlined" type="submit" loading={submitLoading} />
                 </main>
             </form>
-            <Modal
-                isOpen={openModal}
-                onClose={() => setOpenModal(null)}
-            >
+            <Modal isOpen={openModal} onClose={() => setOpenModal(null)}>
                 <TextGuide text={text.alertSuicidalThoughts} />
             </Modal>
-
         </>
-    )
+    );
 }
